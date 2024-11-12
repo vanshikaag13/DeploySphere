b19d7880acee47f32d0d0125fc86422451e71ad9
vanshikaag13 <agrawalvanshi286@gmail.com>
2024-11-13 03:07:06 +0530

    added eks config

---
+++ eks.tf
@@ -0,0 +1,11 @@
+
+resource "aws_eks_cluster" "jenkins_cluster" {
+  name     = "jenkins-eks"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnet[*].id
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_policy]
+}
